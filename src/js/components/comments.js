c27552ad4ff165ada41b324513a7d032bd32830c
lantuhui <shengxiaobai15@163.com>
2017-10-25 09:09:54 +0800

    finish comments component
    finish comments component

--- src/js/components/comments.js
+++ src/js/components/comments.js
@@ -36,7 +36,21 @@ class Comments extends React.Component {
             })
     }
 
+    handleSubmit(e){
+        e.preventDefault();
+        var formdata = this.props.form.getFieldsValue();
+        axios.get("http://newsapi.gugujiankong.com/Handler.ashx?action=comment&userid=" + localStorage.userid + "&uniquekey=" + this.props.uniquekey + "&commnet=" + formdata.remark)
+            .then((response) => {
+                this.componentDidMount();
+            })
+    }
+
+    addUserCollection(){
+        notification.success({message: 'ReactNews提醒',description: '收藏此文章成功'});
+    }
+
     render(){
+        let { getFieldDecorator } = this.props.form;
         const { comments } = this.state;
         const commentList = comments.length
             ?comments.map((comment,index) => (
@@ -49,6 +63,16 @@ class Comments extends React.Component {
                 <Row>
                     <Col span={24}>
                         { commentList }
+                        <Form onSubmit={this.handleSubmit.bind(this)}>
+                            <FormItem label="您的评论">
+                                {getFieldDecorator('remark',{})(
+                                    <Input placeholder="随便写"/>
+                                )}
+                            </FormItem>
+                            <Button type="primary" htmlType="submit">提交评论</Button>
+                            &nbsp;&nbsp;
+                            <Button type="primary" htmlType="button" onClick={this.addUserCollection.bind(this)}>收藏文章</Button>
+                        </Form>
                     </Col>
                 </Row>
             </div>
