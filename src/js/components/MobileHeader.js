b387f3cb4df6e98eafe3127d6f312838c3270143
lantuhui <shengxiaobai15@163.com>
2017-10-26 08:01:49 +0800

    create MobileHeader
    create MobileHeader

---
+++ src/js/components/MobileHeader.js
@@ -0,0 +1,29 @@
+/**
+ * Created by lantu on 2017/10/26.
+ */
+
+import React from 'react';
+import { Row,Col } from 'antd';
+import {
+    Menu,
+    Icon,
+    Tabs,
+    message,
+    Form,
+    Input,
+    Button,
+    CheckBox,
+    Modal
+} from 'antd';
+
+class MobileHeader extends React.Component {
+    render(){
+        return(
+            <div>
+                黄茜
+            </div>
+        )
+    }
+}
+
+export default Form.create({})(MobileHeader);
--- src/js/components/MobileIndex.js
+++ src/js/components/MobileIndex.js
@@ -3,11 +3,13 @@
  */
 
 import React from 'react'
+import MobileHeader from './MobileHeader'
 
 export default class extends React.Component {
     render(){
         return (
             <div>
+                <MobileHeader></MobileHeader>
                 huangqian
             </div>
         )
