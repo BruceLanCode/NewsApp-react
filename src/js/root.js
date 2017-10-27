4e9a2c63c2af21d493a416f12a84335b2186cb9e
lantuhui <shengxiaobai15@163.com>
2017-10-27 09:00:22 +0800

    create usercenter page
    create usercenter page

--- src/js/root.js
+++ src/js/root.js
@@ -9,6 +9,7 @@ import PCNewsDetail from './components/pc_news_detail';
 import PCUserCenter from './components/pc_usercenter';
 import MobileIndex from './components/MobileIndex';
 import MobileNewsDetail from './components/mobile_news_detail'
+import MobileUserCenter from './components/mobile-usercenter'
 import '../global_css/antd.css';
 import '../global_css/antd_improve.css';
 import '../global_css/pc.css';
@@ -31,6 +32,7 @@ export default class Root extends React.Component {
                         <div>
                             <Route exact path="/" component={MobileIndex}></Route>
                             <Route path="/details/:uniquekey" component={ MobileNewsDetail }></Route>
+                            <Route path="/usercenter" component={MobileUserCenter}></Route>
                         </div>
                     </Router>
                 </MediaQuery>
