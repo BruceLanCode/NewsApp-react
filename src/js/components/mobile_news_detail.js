f13d2ea4b63a370ae14014c19566514e7e62edb8
lantuhui <shengxiaobai15@163.com>
2017-10-27 08:49:26 +0800

    finish mobile detail page
    finish mobile detail page

--- src/js/components/mobile_news_detail.js
+++ src/js/components/mobile_news_detail.js
@@ -3,11 +3,49 @@
  */
 
 import React from 'react';
+import { Row,Col,BackTop } from 'antd';
+import MobileHeader from './MobileHeader'
+import MobileFooter from './MobileFooter'
+import CommonComments from './comments';
+
+import axios from 'axios';
 
 export default class extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            newsItem: ''
+        }
+    }
+
+    componentDidMount(){
+        axios.get("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.match.params.uniquekey)
+            .then((response) => {
+                this.setState({newsItem: response.data});
+                document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台";
+            })
+    }
+
+    createMarkup(){
+        return { __html: this.state.newsItem.pagecontent };
+    }
+
     render(){
         return (
-            <div>huangqian</div>
+            <div id="mobileDetailsContainer">
+                <MobileHeader></MobileHeader>
+                <div className="ucmobileList">
+                    <Row>
+                        <Col span={24} className="container">
+                            <div className="articleContainer" dangerouslySetInnerHTML={this.createMarkup()}></div>
+                            <hr/>
+                            {/*<CommonComments uniquekey={this.props.match.params.uniquekey}></CommonComments>*/}
+                        </Col>
+                    </Row>
+                </div>
+                <MobileFooter></MobileFooter>
+                <BackTop></BackTop>
+            </div>
         )
     }
 }
